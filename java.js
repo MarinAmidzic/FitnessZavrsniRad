f6c018618f35c279b62db4d2f966c7e86a5ab3e3
MarinAmidzic <73863784+MarinAmidzic@users.noreply.github.com>
2021-01-26 21:49:49 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+
+
+
+(function () {
+    
+    const loginbtn = document.getElementById('loginbtn');
+    const logincontainer = document.getElementById('logincontainer');
+    
+    loginbtn.addEventListener('click', function(){
+        logincontainer.classList.toggle('show')
+    })
+     
+        
+    })();
+
+
+    /*JAVASCRIPT UVIJEK NA DNO STRANICE NA KRAJU BODYA JER AKO SE UCITAVA IZ HEADA ONDA SE SKRIPTA POKRENE PRIJE NEGO SE UCITA STRANICA I ONDA MI NE RADI JAVA */
+
